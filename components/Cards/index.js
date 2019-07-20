5d795325cd2b17a480dc3c2aa3380894a5bef0c3
Denvinn Magsino <denvinnmagsino@denvinns-air.home>
2019-07-20 11:57:25 -0700

    finsihed sprint

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,66 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(axiosData=>{
+        
+        console.log(axiosData.data.articles)
+        const cardsCon = document.querySelector('.cards-container');
+        axiosData.data.articles.javascript.forEach(card => {
+
+            cardsCon.appendChild(cardMaker(card));
+        });
+        axiosData.data.articles.bootstrap.forEach(card => {
+
+            cardsCon.appendChild(cardMaker(card));
+        });
+        axiosData.data.articles.jquery.forEach(card => {
+
+            cardsCon.appendChild(cardMaker(card));
+        });
+        axiosData.data.articles.technology.forEach(card => {
+
+            cardsCon.appendChild(cardMaker(card));
+        });
+        axiosData.data.articles.node.forEach(card => {
+
+            cardsCon.appendChild(cardMaker(card));
+        });
+
+    })
+    .catch(err=>{
+        console.log("ERROR 404 PAGE NOT FOUND");
+    })
+
+    
+
+    function cardMaker(card){
+        let cardHolder = document.createElement('div');
+        let headline=document.createElement('div');
+        let author = document.createElement('div');
+        let imgCon = document.createElement('div');
+        let img = document.createElement('img');
+        let autNam = document.createElement('span');
+
+        cardHolder.classList.add('card');
+        headline.classList.add('headline');
+        author.classList.add('author');
+        imgCon.classList.add('img-container');
+
+
+        headline.innerHTML=card.headline;
+        img.src=card.authorPhoto;
+        autNam.innerHTML=card.authorName;
+
+
+        cardHolder.appendChild(headline);
+        cardHolder.appendChild(author);
+        author.appendChild(imgCon);
+        author.appendChild(autNam);
+        imgCon.appendChild(img);
+
+
+        console.log(cardHolder);
+        return cardHolder;
+    };
