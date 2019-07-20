7df5ea63419ec3794d0b8efd91f3ffae4a44c34f
Denvinn Magsino <denvinnmagsino@denvinns-air.home>
2019-07-20 09:27:27 -0700

    header done

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+
+function Header() {
+    let header=document.createElement('div');
+    let date=document.createElement('span');
+    let titl=document.createElement('h1');
+    let temp=document.createElement('span');
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    date.innerHTML="SMARCH 28, 2019";
+    titl.innerHTML="Lambda Times";
+    temp.innerHTML="98";
+    
+    header.appendChild(date);
+    header.appendChild(titl);
+    header.appendChild(temp);
+
+    const headerContainer = document.querySelector('.header-container');
+    headerContainer.appendChild(header);
+    return headerContainer;
+}
+
+Header();
